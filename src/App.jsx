4ba13e8a90f676663352bd0c4a297c374b990bfe
Joane <138986247+japcunha@users.noreply.github.com>
2025-04-21 20:32:15 -0300

    Adc estilização do navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Navbar from "./components.jsx/Navbar";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return <>
-  <Navbar/>
-  <h1 className="text-2xl font-bold">hello world</h1>
-  </>;
+
+export default function App() {
+  return (
+    <div className="bg-[url('/pexels-chevanon-312418.jpg')] bg-cover bg-center min-h-screen">
+    <Navbar />
+      <h1 className="text-lime-50 text-2xl font-bold  text-center pt-20">Bem vindo a casa tua café</h1>
+    </div>
+  );
 }
 
-export default App;
+//alterações: navbar, background e home.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
+
 export default function Navbar() {
-    return (
-        <header className="">
-            <div className="">
-                <h1>
-                casa tua café
-                </h1>
-                <nav>
-                    
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header className="bg-emerald-900 text-white shadow-md">
+      <div className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
+        <h1>casa tua café</h1>
+        <nav>
+          <ul className="flex space-x-6 font-medium">
+            <li>
+              <a href="#menu" className="text-lime-50 hover:underline transition duration-300 ease-in-out">sobre</a>
+            </li>
+            <li>
+              <a href="#sobre" className="text-lime-50 hover:underline transition duration-300 ease-in-out">menu</a>
+            </li>
+            <li>
+              <a href="#contado" className="text-lime-50 hover:underline transition duration-300 ease-in-out">contato</a>
+            </li>
+            <li>
+              <a href="login" className="text-lime-50 hover:underline transition duration-300 ease-in-out"> login</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
