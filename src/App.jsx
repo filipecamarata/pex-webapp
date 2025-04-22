d81f2353fb1e7a51be8fac7c672b16ec3e0cdc66
Joane <138986247+japcunha@users.noreply.github.com>
2025-04-21 21:50:18 -0300

    Adc footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Products from "./components/Products.jsx";
 import Home from "./pages/Home.jsx";
 import Menu from "./pages/Menu.jsx";
@@ -12,9 +13,15 @@ export default function App() {
     <Navbar />
     <h1 className="text-lime-50 text-2xl font-bold  text-center pt-20">Bem vindo(a) a <span className="text-amber-100">Casa Tua Café</span></h1>
     <Products/>
-
+    <div className="text-center mt-5">
+      <button className="bg-amber-100 text-black font-semibold px-6 py-2 rounded-full shadow-md hover:bg-gray-200 transition duration-300">
+      veja mais
+      </button>
+      <Footer/>
+    </div>
     </div>
   );
 }
 
-//alterações: navbar, background e home.
+
+//adiçao de botão 'veja mais' e o footer
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+export default function Footer(){
+    return (
+        <footer className="bg-emerald-950 text-white text-center py-7 mt-10">
+            <p className="text-sm">&copy; {new Date().getFullYear()} </p>
+                <p className="text-sm">Café Casa Tua</p>
+        </footer>
+    )
+}
